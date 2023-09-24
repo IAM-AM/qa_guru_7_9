238a8bf21e93c00092874abd8a0a2b5fbae5b7ed
IAM-AM <aliaksandr.manko1@gmail.com>
2023-09-24 23:57:06 +0400

    initial commit

--- tests/test_github_issue_selene.py
+++ tests/test_github_issue_selene.py
@@ -1,17 +1,23 @@
-from selene import browser, by, be
 import allure
+from selene import browser, by, be
+from selene.support.shared.jquery_style import s
+from allure_commons.types import Severity
 
 
-def test_github():
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.feature("Issues in Repository")
+@allure.story("Created issue is shown")
+@allure.link("https://github.com/", name='Testing')
+@allure.label('owner', 'IAM-AM')
+def test_github_issue_selene():
     browser.open("https://github.com")
 
-    browser.element(".search-input").click()
-    browser.element(".QueryBuilder-InputWrapper").type(
-        'eroshenkoam/allure-example').press_enter()
+    s(".search-input").click()
+    s('//*[@id="query-builder-test"]').type("eroshenkoam/allure-example").press_enter()
 
-    browser.element('[data-testid=results-list]').element(
-        by.link_text('eroshenkoam/allure-example')).click()
+    s('[data-testid=results-list]').element(by.link_text('eroshenkoam/allure-example')).click()
 
-    browser.element('#issues-tab').click()
+    s('#issues-tab').click()
 
-    browser.element(by.partial_text('#81')).should(be.visible)
+    s(by.partial_text('#81')).should(be.visible)
